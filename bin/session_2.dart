2431c685bbbb43e4a6637ceb67d528a24513a7df
Mustafa Salman <frostnote2@gmail.com>
2024-02-10 15:45:43 +0800

    Initial Commit

---
+++ bin/session_2.dart
@@ -0,0 +1,5 @@
+import 'package:session_2/session_2.dart' as session_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${session_2.calculate()}!');
+}
---
+++ test/session_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:session_2/session_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
